7153b3ad37c6e8ac080cfebb8cc40612766126f9
samgrs <samilagrodrigues@hotmail.com>
2021-09-17 14:00:55 -0300

    projeto semana 5

--- projeto/database.js
+++ projeto/database.js
@@ -1,30 +1,202 @@
 //cria o array de objetos estudantes 
 const livros = [
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-    {
-      nome: 'incluir'
-    },
-]
+  {
+    name: 'Cosmos - Carl Sagan',
+    type: 'nao-ficcao',
+    pages: 560,
+    recommends: true,
+    readed: true
 
+  },
+  {
+    name: 'Moby Dick - Herman Melville',
+    type: 'romance',
+    pages: 656,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Crime e Castigo - Dostoiévski',
+    type: 'romance',
+    pages: 608,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Sapiens: Uma Breve História da Humanidade - Yuval Harari',
+    type: 'nao-ficcao',
+    pages: 464,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Torto Arado - Itamar Vieira Junior',
+    type: 'romance',
+    pages: 264,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'A Hora da Estrela - Clarice Lispector',
+    type: 'romance',
+    pages: 88,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Água Viva - Clarice Lispector',
+    type: 'romance',
+    pages: 96,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'O Pequeno Príncipe - Antoine de Saint-Exupéry',
+    type: 'romance',
+    pages: 143,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Mrs. Dalloway - Virginia Woolf',
+    type: 'romance',
+    pages: 328,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Macbeth - Shakespeare',
+    type: 'tragedia',
+    pages: 128,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Hamlet - Shakespeare',
+    type: 'tragedia',
+    pages: 320,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Noites Brancas - Dostoiévski',
+    type: 'romance',
+    pages: 96,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'O Apanhador no Campo de Centeio J.D. Salinger',
+    type: 'romance',
+    pages: 250,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Cem Anos de Solidão - Gabriel García Márquez (Gabo)',
+    type: 'romance',
+    pages: 16,
+    recommends: 448,
+    readed: true
+  },
+  {
+    name: 'O Amor nos Tempos do Cólera - Gabo',
+    type: 'romance',
+    pages: 432,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'O Homem sem Talento - Tsuge Yoshiharu',
+    type: 'watakushi',
+    pages: 240,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Dom Casmurro - Machado de Assis',
+    type: 'romance',
+    pages: 400,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Sidarta - Herman Hesse',
+    type: 'romance',
+    pages: 176,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'A Metamorfose - Kafka',
+    type: 'novela',
+    pages: 232,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'O Velho e o Mar - Ernest Hemingway',
+    type: 'romance',
+    pages: 126,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Fahrenheit 451 - Ray Bradbury',
+    type: 'romance',
+    pages: 216,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'O Retrato de Dorian Gray - Oscar Wilde',
+    type: 'romance',
+    pages: 264,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'O Morro dos Ventos Uivantes - Emily Bronte',
+    type: 'romance',
+    pages: 376,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Orgulho e Preconceito - Jane Austen',
+    type: 'romance',
+    pages: 424,
+    recommends: true,
+    readed: true
+  },
+  {
+    name: 'Homo Deus - Yuval Harari',
+    type: 'nao-ficcao',
+    pages: 448,
+    recommends: true,
+    readed: false
+  },
+  {
+    name: 'A Náusea - Jean-Paul Sartre',
+    type: 'romance',
+    pages: 204,
+    recommends: true,
+    readed: false
+  },
+  {
+    name: 'Paraíso Perdido - John Milton',
+    type: 'poema',
+    pages: 896,
+    recommends: true,
+    readed: false
+  },
+  {
+    name: 'Frankestein - Mary Shelley',
+    type: 'romance',
+    pages: 304,
+    recommends: true,
+    readed: false
+  },
+]
 //exporta o array de objetos estudantes
 module.exports = livros
--- projeto/index.js
+++ projeto/index.js
@@ -1,9 +1,71 @@
-console.log('--------------------------------------')
-console.log('     Projeto            ')
-console.log('             Tassila Bomfim         ')
-console.log('--------------------------------------')
+console.log('-----------------------------------------------------------------------------------')
+console.log('                     WELCOME!       ')
+console.log('-----------------------------------------------------------------------------------')
+console.log('Aqui estão alguns livros lidos e minha wishlist!.')
+console.log('-----------------------------------------------------------------------------------')
 
 
 //vamos instanciar, o nosso banco de dados
+const books = require('./database');
 
+//ordenando
+books.sort((x, y) => x.pages - y.pages);
+console.table(books);
 
+//entrada de dados
+const entry = require('readline-sync');
+
+//pergunta no terminal
+const booksQuestion = entry.question('Gostaria de ver os livros por categoria? (S/N)? ').toLocaleUpperCase();
+console.log(booksQuestion);
+
+//se afirmativo:
+if(booksQuestion === 'S') {
+    console.log('Insira o tipo (romance, nao-ficcao, tragedia, poema, novela, watakushi). Escreva tudo minúsculo e sem acentos ou "ç". ');
+
+    //exibe no terminal
+    const genre = entry.question('Insira o gênero que deseja ver: ').toLocaleLowerCase();
+    console.log('Livros selecionados:');
+
+    function tagging(books) {
+        return books.type === genre
+    }
+
+    const filterBooks = books.filter(tagging);
+    console.table(filterBooks);
+
+    //recomendacao
+    const recommendationBooks = entry.question('Gostaria de uma recomendação de livro (S/N)? ').toLocaleUpperCase();
+    console.log('Livros recomendados:');
+
+if(recommendationBooks  === 'S') {
+    function recommendation(books) {
+        return books.recommends && books.readed === true
+    }
+
+    const answerRecommendation = books.filter(recommendation);
+    console.table(answerRecommendation);
+} else {
+    console.log('Leia sempre! <3');
+}
+    const readBooks = entry.question('Gostaria de ver minha wishlist (S/N)? ').toLocaleUpperCase();
+    console.log('Minha wishlist do momento:');
+
+if(readBooks === 'S'){
+
+    function notReaded(books) {
+        return books.readed === false
+    }
+
+    const wishList = books.filter(notReaded);
+    console.table(wishList);
+} else {
+    console.log('Leia sempre!<3');
+}
+} else if(booksQuestion === 'N'){
+    console.log('Leia sempre! <3');
+   
+} else {
+    console.log('Todos os livros catalogados: ');
+    console.table(books);
+}
